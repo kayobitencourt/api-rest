a0c0c07608bc1b64322d938b1d332009033769af
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-06-25 19:42:26 -0300

    retornando a resposta em formato json e status code

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,8 @@ app.get('/products', (request, response) => {
 app.post('/products', (request, response) => {
   const { name, price } = request.body;
 
-  response.send(`Produto ${name}, custo exatamente ${price}`);
+  // response.send(`Produto ${name}, custo exatamente ${price}`);
+  response.status(201).json({ name, price });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
