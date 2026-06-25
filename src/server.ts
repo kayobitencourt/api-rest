568e3231d6f27bc02bcd79bcb8317cd8f1dd3bb6
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-06-25 19:23:47 -0300

    Route params com express

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ const PORT = 3333; //Em caixa alta quando é algo de configuração
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello World Express');
+app.get('/products/:id', (request, response) => {
+  const { id } = request.params;
+  response.send(` Produto ${id}`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
