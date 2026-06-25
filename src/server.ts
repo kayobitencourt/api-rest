e3baf70f947bfa55295c994ef5041e792468f7a3
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-06-25 19:20:31 -0300

    Enviando a requisição com o método GET

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,8 @@ const PORT = 3333; //Em caixa alta quando é algo de configuração
 
 const app = express();
 
+app.get('/', (request, response) => {
+  response.send('Hello World Express');
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
