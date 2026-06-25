34753d271ee44b84a1644a147be8d8bf58e47c82
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-06-25 19:29:17 -0300

    Query params com node/express

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,10 @@ const PORT = 3333; //Em caixa alta quando é algo de configuração
 
 const app = express();
 
-app.get('/products/:id', (request, response) => {
-  const { id } = request.params;
-  response.send(` Produto ${id}`);
+app.get('/products', (request, response) => {
+  const { page, limit } = request.query;
+  // /products?page=1&limit=10
+  response.send(`${page}e${limit}`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
