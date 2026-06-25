a52bb00a3ee130be08affe560b4a019771e5ee2e
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-06-25 19:39:20 -0300

    POST com middlaware express.json()

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,17 @@ import express from 'express';
 const PORT = 3333; //Em caixa alta quando é algo de configuração
 
 const app = express();
+app.use(express.json());
 
 app.get('/products', (request, response) => {
   const { page, limit } = request.query;
-  // /products?page=1&limit=10
   response.send(`${page}e${limit}`);
+});
+
+app.post('/products', (request, response) => {
+  const { name, price } = request.body;
+
+  response.send(`Produto ${name}, custo exatamente ${price}`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
