989e3520ae8d31f0bd06c5dd084126ead11f5579
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2026-06-25 22:55:34 -0300

    Regras de validação avançada com zod

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -18,8 +18,8 @@ class ProductsController {
 
   create(request: Request, response: Response) {
     const bodySchema = z.object({
-      name: z.string({ required_error: 'Name is required' }),
-      price: z.number({ required_error: 'Price is required' }),
+      name: z.string({ required_error: 'Name is required' }).trim().min(6, { message: 'Name Must be 6 or more characters' }),
+      price: z.number({ required_error: 'Price is required' }).positive({ message: 'Price must be positive' }),
     });
 
     const { name, price } = bodySchema.parse(request.body);
